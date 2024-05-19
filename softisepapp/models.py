65eff63179d042572e43e322b8a62fda93fa0b45
Tom Amar <95186955+TarobaseM@users.noreply.github.com>
2024-05-20 00:14:48 +0200

    amelioration page logiciel + ajout dun favicon

--- softisepapp/models.py
+++ softisepapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from datetime import date
 # Create your models here.
 from django.db import models
 
@@ -18,6 +18,9 @@ class Cours(models.Model):
 class Logiciel(models.Model):
     description = models.TextField(default="lorem ipsum")
     nom = models.CharField(max_length=100)
+    version = models.CharField(max_length=50,default="1.0")
+    date_de_sortie = models.DateField(default=date(2002, 11, 10))
+    systeme_exploitation = models.CharField(max_length=100,default="Windows")
     url_telechargement = models.URLField()
     
     def __str__(self):
